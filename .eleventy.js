bcb414a7abd1ecde53ea2c499c8a5ac045df81a1
kellegram <kellegram@protonmail.ch>
2020-08-22 17:15:45 +0000

    Start building with eleventy

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+module.exports = function(eleventyConfig) {
+
+	eleventyConfig.addPassthroughCopy("assets");
+
+
+
+	return {
+		passthroughFileCopy = true,
+		dir: {
+			input: "src",
+			output: "_site",
+			include: "includes",
+			}
+
+		} 
+
+}
