25fac9077b4f21d123c4533ede5544514a489929
kellegram <kellegram@protonmail.ch>
2020-08-28 23:57:57 +0000

    glow

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,6 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
-<<<<<<< HEAD
-  eleventyConfig.addPassthroughCopy("styles");
-=======
   eleventyConfig.addPassthroughCopy("src/styles");
->>>>>>> 6c9af9b18e13fa470d62b3a9e07ec444ff38dc46
   return {
     passthroughFileCopy: true,
     templateFormats: ["html", "md", "liquid"],
