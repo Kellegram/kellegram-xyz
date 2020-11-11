8ec67b2ce961d7e208757e5ec9d531a1877df215
kellegram <44496474+Kellegram@users.noreply.github.com>
2020-11-11 21:04:00 +0000

    Update eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,9 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(syntaxHighlight, {
+    alwaysWrapLineHighlights: false,
+  });
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("src/styles");
   eleventyConfig.addPassthroughCopy("src/assets");
